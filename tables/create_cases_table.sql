5a0f32ce11bfbb7b91a09459bb23f0fcd3bf4e9e
etheleon <picy2k@gmail.com>
2024-10-01 15:35:18 +0800

    Update create_cases_table.sql

--- tables/create_cases_table.sql
+++ tables/create_cases_table.sql
@@ -7,9 +7,9 @@ CREATE TABLE IF NOT EXISTS national_analysis.dengue_fever_cases (
     postal INTEGER
 );
 
-COMMENT ON COLUMN national_analysis.df_cases.case_id IS 'Unique identifier for each case';
-COMMENT ON COLUMN national_analysis.df_cases.cluster_id IS 'Unique identifier of the associated cluster';
-COMMENT ON COLUMN national_analysis.df_cases.date IS 'Onset date of the case';
-COMMENT ON COLUMN national_analysis.df_cases.serotype IS 'Serotype of the dengue case could be, could be multiple serotype due to paste infections';
-COMMENT ON COLUMN national_analysis.df_cases.residential IS 'Residential information of the case';
-COMMENT ON COLUMN national_analysis.df_cases.postal IS 'Postal code of the residence';
+COMMENT ON COLUMN national_analysis.dengue_fever_cases.case_id IS 'Unique identifier for each case';
+COMMENT ON COLUMN national_analysis.dengue_fever_cases.cluster_id IS 'Unique identifier of the associated cluster';
+COMMENT ON COLUMN national_analysis.dengue_fever_cases.date IS 'Onset date of the case';
+COMMENT ON COLUMN national_analysis.dengue_fever_cases.serotype IS 'Serotype of the dengue case could be, could be multiple serotype due to paste infections';
+COMMENT ON COLUMN national_analysis.dengue_fever_cases.residential IS 'Residential information of the case';
+COMMENT ON COLUMN national_analysis.dengue_fever_cases.postal IS 'Postal code of the residence';
