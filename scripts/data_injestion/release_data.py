c689462b8f4f69032e0e77066f96fa968de0ab1a
etheleon <picy2k@gmail.com>
2024-10-08 14:50:31 +0800

    :wrench: Update release site
    * script wasnt working
    * update SQL file to follow naming convention

--- scripts/data_injestion/release_data.py
+++ scripts/data_injestion/release_data.py
@@ -1,48 +1,62 @@
+#!/usr/bin/env python
 """Script for injesting release data."""
-
-# TODO(Wesley): clean this up (not tested)
 
 from os.path import join
 
-from utils import read_excel_file
-
-release_site_home = "/home/wesley/github/etheleon/national_analysis/data/release_site"
-
-hdb_df = read_excel_file(join(release_site_home, "hdb.xlsx"))
-hdb_df["PremiseType"].unique()
+import numpy as np
+import pandas as pd
+from utils import read_excel_file, upsert_dataframe_to_db
 
 
-def insert_data_into_postgresql(df, connection):
-    """Helper to add data to DB.
-
-    Args:
-        df (pd.DataFrame): The dataframe to be upserted
-        connection: the connection to db
-    """
-    cursor = connection.cursor()
-
-    insert_query = sql.SQL(
-        """
-        INSERT INTO national_analysis.site_release (
-            postal, sector_id, premise_type, release_date, total_dwelling
-        ) VALUES (%s, %s, %s, %s, %s)
-    """
+def parse_release_site_data(data_root: str, file="hdb.xlsx") -> pd.DataFrame:
+    """Reads release site data."""
+    df = read_excel_file(join(data_root, file))
+    df = df[["Postal", "PremiseType", "Sector_ID", "FirstSustainedReleaseDate_Postal", "TotalDwelling"]].rename(
+        columns={
+            "Postal": "postal",
+            "PremiseType": "premise_type",
+            "Sector_ID": "sector_id",
+            "FirstSustainedReleaseDate_Postal": "release_date",
+            "TotalDwelling": "total_dwelling",
+        }
     )
-
-    for index, row in df.iterrows():
-        cursor.execute(
-            insert_query,
-            (
-                int(row["postal"]),
-                row["sector_id"],
-                row["premise_type"],
-                row["release_date"] if pd.notnull(row["release_date"]) else None,  # Handle NA dates
-                int(row["total_dwelling"]) if pd.notnull(row["total_dwelling"]) else None,
-            ),
-        )
-
-    connection.commit()
-    print("Data inserted successfully into national_analysis.site_release!")
+    df = df.assign(release_date=lambda x: np.where(pd.notnull(x.release_date), x.release_date, None))
+    df = df.assign(release_date=lambda x: pd.to_datetime(x.release_date, errors="coerce", unit="ns"))
+    df = df.assign(total_dwelling=lambda x: np.where(pd.notnull(x.total_dwelling), x.total_dwelling, None))
+    df = df.groupby(["postal", "premise_type", "sector_id", "release_date"])["total_dwelling"].sum().reset_index()
+    return df
 
 
-insert_data_into_postgresql(hdb_df_short, connection)
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Release site data")
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        default="./data/release_site",
+        help="Directory containing the release_site CSV file",
+    )
+    parser.add_argument(
+        "--secret",
+        type=str,
+        default=".secrets.toml",
+        help="secrets file",
+    )
+    parser.add_argument(
+        "--ddl_root",
+        type=str,
+        default="./tables/create_release_site_table.sql",
+        help="dir for keeping DDL",
+    )
+    args = parser.parse_args()
+
+    df = pd.concat(
+        [
+            parse_release_site_data(args.data_dir, "hdb.xlsx"),
+            parse_release_site_data(args.data_dir, "landed.xlsx"),
+            parse_release_site_data(args.data_dir, "rct.xlsx"),
+        ]
+    )
+    print(df)
+    upsert_dataframe_to_db(df=df, ddl_file=args.ddl_root, table_name="site_release", connection_params_path=args.secret)
